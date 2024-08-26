95cd33a74aba6ca2364a64c68ee5dc952af6048f
Piloxarp <piloxarp@gmail.com>
2024-08-26 22:55:54 +0300

    0.2

--- js/index.js
+++ js/index.js
@@ -50,3 +50,34 @@ document.getElementById('ThemeBTN').addEventListener('click', function() {
         currentIndex = 0; 
     }
 });
+
+let language = window.navigator.language;
+let languageFistTwo = language.substr(0,2); // To only keep the first 2 characters.
+
+function LangTransform(lang) {
+  console.log(document.querySelectorAll(".head_case"));
+  console.log(document.querySelectorAll(".projects > a > p"))
+  console.log(document.querySelectorAll(".fg_btn"))
+  if(lang == 'ru'){
+  document.querySelectorAll(".head_case > h2")[0].innerHTML = "Ссылки:";
+  document.querySelectorAll(".head_case > h2")[1].innerHTML = "Проекты:";
+  document.querySelectorAll(".projects > a > p")[0].innerHTML = "Описание";
+  document.querySelectorAll(".projects > a > p")[1].innerHTML = "Описание";
+  document.querySelectorAll(".projects > a > p")[2].innerHTML = "Описание";
+  document.querySelector(".fg_btn").innerHTML = "ЛЮБИМЫЕ ИГРЫ";
+  } else {
+
+  }
+}
+
+
+switch (languageFistTwo) { 
+  case "en":
+   if(languageFistTwo = 'en'){
+   }
+
+  default:
+     if(languageFistTwo = 'ru'){
+      //LangTransform("ru"); // Active RU language
+     }
+} 
